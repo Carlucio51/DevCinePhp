d77a50e04c2e55b9317a5fbcb3536fa0ee76f584
Carlúcio Filho <carlucioavelino51@gmail.com>
2021-06-30 21:53:58 -0300

    Feito o uso do PHP pra que ele monte automaticamente os cards baseado em dados que podem ser flexíveis.

--- galeria.php
+++ galeria.php
@@ -1,5 +1,37 @@
 <?php include "cabecalho.php" ?>
 
+<?php
+
+$filme1 = [
+    "titulo" => "Vingadores: Ultimato",
+    "nota" => 8.6,
+    "sinopse" => "Após os eventos devastadores de Guerra do Infinito, o universo está em ruínas devido aos esforços do Titã Louco, Thanos. Com a ajuda de aliados remanescentes, os Vingadores devem se reunir mais uma vez a fim de desfazer as ações de Thanos e restaurar a ordem no universo de uma vez por todas, não importando as consequências.",
+    "poster" => "https://www.themoviedb.org/t/p/original/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+];
+$filme2 = [
+    "titulo" => "Ad Astra",
+    "nota" => 6.5,
+    "sinopse" => "Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.",
+    "poster" => "https://www.themoviedb.org/t/p/original/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg"
+];
+$filme3 = [
+    "titulo" => "O Alto da Compadecida",
+    "nota" => 9.9,
+    "sinopse" => "O Auto da Compadecida: As aventuras dos nordestinos João Grilo (Matheus Natchergaele), um sertanejo pobre e mentiroso, e Chicó (Selton Mello), o mais covarde dos homens. Ambos lutam pelo pão de cada dia e atravessam por vários episódios enganando a todos do pequeno vilarejo de Taperoá, no sertão da Paraíba. A salvação da dupla acontece com a aparição da Nossa Senhora (Fernanda Montenegro). Adaptação da obra de Ariano Suassuna.",
+    "poster" => "https://www.themoviedb.org/t/p/original/y3IhuITELtprMhUd7bccu629rba.jpg"
+];
+$filme4 = [
+    "titulo" => "Star Wars: Episódio IX - A Ascensão de Skywalker",
+    "nota" => 8.1,
+    "sinopse" => "Com o retorno do Imperador Palpatine, todos voltam a temer seu poder e, com isso, a Resistência toma a frente da batalha que ditará os rumos da galáxia. Treinando para ser uma completa Jedi, Rey (Daisy Ridley) ainda se encontra em conflito com seu passado e futuro, mas teme pelas respostas que pode conseguir a partir de sua complexa ligação com Kylo Ren (Adam Driver), que também se encontra em conflito pela Força.",
+    "poster" => "https://www.themoviedb.org/t/p/original/1cajv97TsZp12DElh9bX9BZkQ9X.jpg"
+];
+
+$filmes = [$filme1, $filme2, $filme3, $filme4];
+
+
+?>
+
 <body>
 
     <nav class="nav-extended purple light-3">
@@ -21,96 +53,28 @@
         </div>
     </nav>
 
-
     <div class="row">
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/original/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
-                    <span class="card-title">Vingadores</span>
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i>
-                    </a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                </div>
-            </div>
-
-        </div>
-
-
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/original/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i>
-                    </a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <span class="card-title">Ad Astra</span>
-
-                    <p>Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.</p>
+        <?php foreach($filmes as $filme) : ?>
+            <div class="col s3">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="<?= $filme["poster"] ?>">
+                        <a class="btn-floating halfway-fab waves-effect waves-light red">
+                            <i class="material-icons">favorite_border</i>
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <p class="valign-wrapper">
+                            <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+                        </p>
+                        <span class="card-title"><?= $filme["titulo"] ?></span>
+                        <p><?= $filme["sinopse"] ?></p>
+                    </div>
                 </div>
             </div>
+        <?php endforeach ?>
 
-        </div>
-
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/original/y3IhuITELtprMhUd7bccu629rba.jpg">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i>
-                    </a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <span class="card-title">O Alto da Compadecida</span>
-
-                    <p>O Auto da Compadecida: As aventuras dos nordestinos João Grilo (Matheus Natchergaele),
-                        um sertanejo pobre e mentiroso, e Chicó (Selton Mello), o mais covarde dos homens.
-                        Ambos lutam pelo pão de cada dia e atravessam por vários episódios enganando a todos
-                        do pequeno vilarejo de Taperoá, no sertão da Paraíba. A salvação da dupla acontece com a aparição
-                        da Nossa Senhora (Fernanda Montenegro). Adaptação da obra de Ariano Suassuna..</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/original/1cajv97TsZp12DElh9bX9BZkQ9X.jpg">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i>
-                    </a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <span class="card-title">Star Wars: Episódio IX A Ascensão de Skywalker </span>
-
-                    <p>Com o retorno do Imperador Palpatine, todos voltam a temer seu poder e, com isso,
-                         a Resistência toma a frente da batalha que ditará os rumos da galáxia. 
-                         Treinando para ser uma completa Jedi,
-                         Rey (Daisy Ridley) ainda se encontra em conflito com seu passado e futuro,
-                         mas teme pelas respostas que pode conseguir a partir de sua complexa ligação
-                         com Kylo Ren (Adam Driver), que também se encontra em conflito pela Força.</p>
-                </div>
-            </div>
-
-        </div>
+    </div>
 
     </div>
 
